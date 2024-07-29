4bcc3075b26c53b32972af85b58e96f57f573187
William Teasley <william.teasley@eonesolutions.com>
2024-07-29 18:03:11 -0500

    Update to styling and added filter

--- src/Pages/PoetryPage/PoetryPage.jsx
+++ src/Pages/PoetryPage/PoetryPage.jsx
@@ -8,6 +8,7 @@ import SubmitPoem from "../../Components/SubmitPoem/SubmitPoem";
 import FilterByAuthor from "../../Components/FilterByAuthor/FilterByAuthor.jsx"; // Import the new modal
 import { updatePoemContent } from "../../redux/app-state-slice";
 import { VerseCollection } from "../../utils/poemObjects.js";
+import axios from 'axios';
 
 const PoetryPage = ({ dispatch, customerData }) => {
     const [showSubmitPoemModal, setShowSubmitPoemModal] = useState(false);
@@ -15,6 +16,22 @@ const PoetryPage = ({ dispatch, customerData }) => {
     const [currentPoemIndex, setCurrentPoemIndex] = useState(0);
     const [filteredPoems, setFilteredPoems] = useState(VerseCollection); // Default to all poems
     const currentPoem = filteredPoems[currentPoemIndex];
+    const sendPoem = async (poemData) => {
+      try {
+        const response = await axios.post('http://poembackend-env-1.eba-xavekrmk.us-east-1.elasticbeanstalk.com/poems/send-email', poemData);
+        console.log('Poem sent successfully:', response.data);
+      } catch (error) {
+        console.error('Error sending poem:', error);
+      }
+    };
+
+    // Example poem data
+    const poemData = {
+        author: "author",
+        date: `2024-07-26T14:45:00.000Z`,
+      title: 'My Poem',
+      poem: 'This is the content of my poem.',
+    };
 
     const openSubmitPoemModal = () => {
         setShowSubmitPoemModal(true);
@@ -61,6 +78,7 @@ const PoetryPage = ({ dispatch, customerData }) => {
             <PoetryPageContainer>
                 <Header customerData={customerData} dispatch={dispatch} />
                 <ButtonContainer>
+                    <Button onClick={() => sendPoem()} label="test"/>
                     <Button onClick={() => nextPoem()} label="Next Poem" />
                     <Button onClick={openSubmitPoemModal} label="Submit Poem" />
                     <Button onClick={openFilterModal} label="Filter By Author" />
